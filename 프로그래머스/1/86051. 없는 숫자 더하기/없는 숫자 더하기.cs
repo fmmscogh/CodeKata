27a29cd56090dbbaf5084c190b9e8b0782939032
fmmscogh <154488249+fmmscogh@users.noreply.github.com>
2024-02-20 10:25:11 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 3.22 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] num = {0,1,2,3,4,5,6,7,8,9};
+        answer = num.Except(numbers).Sum();
+        return answer;
+    }
+}
